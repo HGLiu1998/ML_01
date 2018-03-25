bfbc13e802ac26d951be2e31bcf7f63c7f1b7f78
105062341 <liuarthur1998@gmail.com>
2018-03-25 15:46:05 +0800

    Cost function finished but float percision has problem

--- ex1.py
+++ ex1.py
@@ -1,11 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from decimal import *
 
-def hypothesis(x):
-    return theta[0][0] + theta[1][0] * x
-
-
-
+def hypothesis():
+    return np.dot(X,theta)
+def Cost_func():
+    Cost = hypothesis()-Y
+    sum = 0.0
+    for i in range(0,m):
+        sum += np.square(Cost[i][0])
+    return sum
 data = np.genfromtxt("ex1data1.txt",delimiter=',')
 m = len(data)
 X = np.array(data[0:m,0]).reshape(m,1)
@@ -18,6 +22,7 @@ Y = np.array(data[0:m,1]).reshape(m,1)
 #plt.show()
 X = np.hstack((np.ones((m,1)), X))
 theta = np.array([[-1],[-2]],dtype = np.float)
-print(theta)
-print(hypothesis(2))
 
+#print(getcontext()) 浮點數精度查詢
+
+print(Cost_func())
