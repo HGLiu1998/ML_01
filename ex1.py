330111b3aa27f18be4aa0894dc349c1069edb5bf
105062341 <liuarthur1998@gmail.com>
2018-03-26 01:40:23 +0800

    Gradient descent

--- ex1.py
+++ ex1.py
@@ -3,21 +3,34 @@ import matplotlib.pyplot as plt
 
 data = np.genfromtxt("ex1data1.txt",delimiter=',')
 m = len(data)
-X = np.array(data[0:m,0]).reshape(m,1)
+x = np.array(data[0:m,0]).reshape(m,1)
 Y = np.array(data[0:m,1]).reshape(m,1)
 #plt.style.use('ggplot')
 #plt.plot(X,Y,'b.')
 #plt.show()
-X = np.hstack((np.ones((m,1)), X))
-theta = np.array([[-1],[-2]],dtype = np.float)
+X = np.hstack((np.ones((m,1)), x))
+theta = np.array([[-1],[-2]])
 
 #print(getcontext()) 浮點數精度查詢
 #define hypothesis and Cost function
 def hypothesis():
     return np.dot(X,theta)
 def Cost_func():
-    Cost = hypothesis()-Y
+    Cost = hypothesis()-Y  
     sum = np.sum(np.square(Cost))/(2*m)
     return np.around(sum,decimals = 8)
-print(hypothesis())
-print(Cost_func())
+
+print("beforeGD:",Cost_func())
+
+iteration = 100
+learningrate = 0.01
+def Gradient_descent():
+    global theta
+    for i in range(iteration):
+        loss = hypothesis()-Y
+        G = (X.T).dot(loss)/m #(2*97)*(97*1) 
+        theta = theta - learningrate * G
+    return theta
+Gradient_descent()
+print (theta)
+print("afterGD:",Cost_func())
