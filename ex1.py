2a1a51cc6af087c1bde2390e49eecbd7ffcd3794
105062341 <liuarthur1998@gmail.com>
2018-03-25 13:47:08 +0800

    matplotlib plot data

--- ex1.py
+++ ex1.py
@@ -1,5 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 data = np.genfromtxt("ex1data1.txt",delimiter=',')
 m = len(data)
-X = data[0:m-1,0]
-Y = data[0:m-1,1]
+X = np.array(data[0:m-1,0])
+Y = np.array(data[0:m-1,1])
+#print(type(X))
+#print((X is data[0:m-1,0]))
+plt.plot(X,Y,'ro')
+plt.show()
+
